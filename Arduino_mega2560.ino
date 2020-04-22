91c4a61a7ae21e12cb0b05f94218d44457394fae
jerkennn <51007343+jerkennn@users.noreply.github.com>
2020-04-22 10:34:55 +0200

    Added structs

--- Arduino_mega2560.ino
+++ Arduino_mega2560.ino
@@ -1,54 +1,66 @@
-/*PID PARAMETERS*/
-#define Kp 0.7    //proportional coefficient
-#define Ki 0.2    //integral coefficient
-#define Kd 0.2    //derivative coefficient
   
-/*GLOBAL PID VARIABLES*/
-float previous_error = 0;
-float integral = 0;
-float setpoint = 0;
+struct PID_parametres {
+  float Kp;
+  float Ki;
+  float Kd;
+};
+
+struct regulator_parametres {
+  struct PID_parametres constants;
+  float previous_error;
+  float integral;
+  float setpoint;
+  float regulator_out;
+};
+
+struct motor_parametres {
+  unsigned char pin;
+  unsigned char value;
+};
+
+struct motor {
+  struct motor_parametres left;
+  struct motor_parametres right;
+};
+
+struct regulator_parametres regulator = {0.1, 0.2, 0.3, 0, 0, 0, 0};
+struct motor motor = {2, 255, 3, 255};
 
 float dt = 0.01;
 float angle = 0;
-float regulator_out = 0;
-
-int left_motor = 2;
-int right_motor = 3;
-int left_motor_val = 238;
-int right_motor_val = 255;
   
 void setup() {
   Serial.begin(115200);
   Serial3.begin(9600);
 
-  pinMode(left_motor, OUTPUT);
-  pinMode(right_motor, OUTPUT);
+  pinMode(motor.left.pin, OUTPUT);
+  pinMode(motor.right.pin, OUTPUT);
 }
 
-float PID_controller(float angle);
+float PID_controller(float input_angle);
   
 void loop() {
 
-  analogWrite(left_motor, left_motor_val);
-  analogWrite(right_motor, right_motor_val);
+  analogWrite(motor.left.pin, motor.left.value);
+  analogWrite(motor.right.pin, motor.right.value);
 
-  regulator_out = PID_controller(angle);
+  float regulator_out = PID_controller(angle);
 }
 
 
-float PID_controller(float angle) {
+float PID_controller(float input_angle) {
   /*START DELTA T TIMING*/
   unsigned long my_time = millis();
   
   /*PID CONTROLLER*/
-  float error = setpoint - angle;
-  integral = integral + error*dt;
-  float derivative = (error - previous_error)/dt;
-  float output = (Kp*error + Ki*integral + Kd*derivative);
-  previous_error = error;
+  float error = regulator.setpoint - input_angle;
+  regulator.integral = regulator.integral + error*dt;
+  float derivative = (error - regulator.previous_error)/dt;
   
   /*WAIT FOR DELTA T*/
   while(millis() - my_time < dt*1000);
   
-  return output;
+  regulator.previous_error = error;
+  return (regulator.constants.Kp*error + regulator.constants.Ki*
+      regulator.integral + regulator.constants.Kd*derivative);
 }
